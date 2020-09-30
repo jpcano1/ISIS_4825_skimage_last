59f3e42c5f82fca3b16e6976b819b339659d7d54
Juan Pablo Cano <jp.cano@uniandes.edu.co>
2020-09-30 17:16:15 -0500

    Setup Colab

---
+++ setup_colab.py
@@ -0,0 +1,21 @@
+import os
+import requests
+
+def download_github_content(path):
+    dir = path.rsplit("/", 1)[0]
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+        os.system(f"touch {dir}/__init__.py")
+
+    elif not os.path.exists(f"{dir}/__init__.py"):
+        os.system(f"touch {dir}/__init__.py")
+
+    os.system(f"shred -u {path}")
+    os.system(f"wget https://github.com/jpcano1/ISIS_4825_skimage_last/raw/master/{path} -O {path}")
+
+def setup_workshop():
+    try:
+        download_github_content("utils/visualization.py")
+        print("Workshop enabled successfully!")
+    except Exception as e:
+        raise e
